84e478ef9a98bd28c6f4d158217e1d4a03011ec5
Paolo Abeni <paolo.abeni@gmail.com>
2018-10-24 15:45:51 +0200

    added unix socket control interface

--- xdp_dumb_switch.c
+++ xdp_dumb_switch.c
@@ -5,16 +5,18 @@
 #include <unistd.h>
 #include <errno.h>
 #include <error.h>
-#include <readline/readline.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <linux/bpf.h>
 #include <linux/types.h>
 #include <net/if.h>
 #include <sys/types.h>
+#include <sys/time.h>
+#include <sys/un.h>
 #include <signal.h>
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
+#include <unistd.h>
 
 #include "xdp_switch_common.h"
 
@@ -39,6 +41,9 @@ struct if_status {
 };
 
 struct xdp_status {
+	int ctr_socket;
+	int cl_socket;
+	FILE *cl_file;
 	int cpu_nr;
 	int rx_ports[MAX_IF];
 	int rx_nr;
@@ -50,7 +55,12 @@ struct xdp_status {
 	struct bpf_object *obj;
 	bool interrupted;
 	bool cleanup;
+	bool use_stdio;
 };
+
+#define __file(__x, __file) (__x->use_stdio ? __file : __x->cl_file)
+#define srv_out(...) fprintf(__file(global_status, stdout), __VA_ARGS__)
+#define srv_err(...) fprintf(__file(global_status, stderr), __VA_ARGS__)
 
 struct xdp_status *global_status;
 
@@ -75,8 +85,8 @@ void __if_stats(struct xdp_status *xdp_status, int id)
 		key.saddr = rule->saddr;
 		if (bpf_map_lookup_elem(xdp_status->maps[MAP_EGRESS], &key,
 		    entry)) {
-			fprintf(stderr, "no stats for rule %x %x\n",
-			        key.saddr, key.ifindex);
+			srv_err("no stats for rule %x %x\n", key.saddr,
+			        key.ifindex);
 			continue;
 		}
 
@@ -88,16 +98,15 @@ void __if_stats(struct xdp_status *xdp_status, int id)
 
 	key.saddr = 0;
 	if (bpf_map_lookup_elem(xdp_status->maps[MAP_EGRESS], &key, entry)) {
-		fprintf(stderr, "no stats for rule %x %x\n",
-		        key.saddr, key.ifindex);
+		srv_err("no stats for rule %x %x\n", key.saddr, key.ifindex);
 	} else {
 		for (i = 0; i < xdp_status->cpu_nr; ++i) {
 			drop_pkts = entry[i].pkts;
 			drop_bytes = entry[i].pkts;
 		}
 	}
-	printf("rx %lu:%lu drop %lu:%lu\n", rx_pkts, rx_bytes, drop_pkts,
-	       drop_bytes);
+	srv_out("rx %lu:%lu drop %lu:%lu\n", rx_pkts, rx_bytes, drop_pkts,
+		drop_bytes);
 }
 
 void __if_detach(struct xdp_status *xdp_status, int id)
@@ -110,7 +119,7 @@ void __if_detach(struct xdp_status *xdp_status, int id)
 	for (rule = xdp_status->if_status[id].list; rule; rule = next) {
 		key.saddr = rule->saddr;
 		if (bpf_map_delete_elem(xdp_status->maps[MAP_EGRESS], &key)) {
-			fprintf(stderr, "can't delete rule %x:%x %d %s\n",
+			srv_err("can't delete rule %x:%x %d %s\n",
 			        key.ifindex, key.saddr, errno, strerror(errno));
 		}
 		next = rule->next;
@@ -120,7 +129,7 @@ void __if_detach(struct xdp_status *xdp_status, int id)
 
 	key.saddr = 0;
 	if (bpf_map_delete_elem(xdp_status->maps[MAP_EGRESS], &key)) {
-		fprintf(stderr, "can't delete stats for if %x %d:%s\n",
+		srv_err("can't delete stats for if %x %d:%s\n",
 		        key.ifindex, errno, strerror(errno));
 	}
 	bpf_set_link_xdp_fd(global_status->rx_ports[id], -1, 0);
@@ -130,8 +139,8 @@ void __if_detach(struct xdp_status *xdp_status, int id)
 		xdp_status->rx_ports[i] = xdp_status->rx_ports[i + 1];
 	}
 
-	printf(" switch program detached from interface %d id %d\n",
-	       key.ifindex, id);
+	srv_out(" switch program detached from interface %d id %d\n",
+		key.ifindex, id);
 	xdp_status->rx_nr--;
 }
 
@@ -141,9 +150,27 @@ void init(struct xdp_status *xdp_status)
 		.prog_type	= BPF_PROG_TYPE_XDP,
 		.file		= "xdp_switch.o",
 	};
+	struct sockaddr_un addr;
 	struct bpf_map *map;
 
 	bzero(xdp_status, sizeof(*xdp_status));
+	xdp_status->cl_socket = -1;
+	xdp_status->ctr_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (xdp_status->ctr_socket == -1)
+		error(1, 0, "socket error");
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path, "xdp_dump_switch_ctl", sizeof(addr.sun_path)-1);
+	unlink(addr.sun_path);
+
+	if (bind(xdp_status->ctr_socket, (struct sockaddr*)&addr,
+		 sizeof(addr)) == -1)
+		error(1, 0, "bind error");
+
+	if (listen(xdp_status->ctr_socket, 5) == -1)
+		error(1, 0, "listen error");
+
 	if (bpf_prog_load_xattr(&prog_load_attr, &xdp_status->obj,
 				&xdp_status->prog_fd))
 		error(1, errno, "can't load file %s", prog_load_attr.file);
@@ -163,6 +190,7 @@ void init(struct xdp_status *xdp_status)
 	xdp_status->interrupted = false;
 	global_status = xdp_status;
 	signal(SIGINT, sigint_handler);
+	signal(SIGPIPE, sigint_handler);
 }
 
 int __lookup_if_by_name(const int *list, const char *name, int *ifindex)
@@ -189,13 +217,13 @@ void if_attach(struct xdp_status *xdp_status, const char *name)
 	int id;
 
 	if (xdp_status->rx_nr == MAX_IF) {
-		fprintf(stderr, "already attached max interfaces %d\n", MAX_IF);
+		srv_err("already attached max interfaces %d\n", MAX_IF);
 		return;
 	}
 
 	id = __lookup_if_by_name(xdp_status->rx_ports, name, &key.ifindex);
 	if (id != -1) {
-		fprintf(stderr, "interface %s:%d already attached in pos %d\n",
+		srv_err("interface %s:%d already attached in pos %d\n",
 		        name, key.ifindex, id);
 		return;
 	}
@@ -206,17 +234,17 @@ void if_attach(struct xdp_status *xdp_status, const char *name)
 	bzero(entry, sizeof(entry));
 	if (bpf_map_update_elem(xdp_status->maps[MAP_EGRESS], &key, entry,
 				BPF_ANY)) {
-		fprintf(stderr, "can't init stats for interface %s:%d: %d:%s\n",
+		srv_err("can't init stats for interface %s:%d: %d:%s\n",
 		        name, key.ifindex, errno, strerror(errno));
 		return;
 	}
 	if (bpf_set_link_xdp_fd(key.ifindex, xdp_status->prog_fd, 0) < 0) {
-		fprintf(stderr, "can't attach xdp program to interface %s:%d: "
+		srv_err("can't attach xdp program to interface %s:%d: "
 		        "%d:%s\n", name, key.ifindex, errno, strerror(errno));
 		return;
 	}
-	printf(" switch program attached to interface %s:%d id %d\n",
-	       name, key.ifindex, xdp_status->rx_nr);
+	srv_out(" switch program attached to interface %s:%d id %d\n",
+		name, key.ifindex, xdp_status->rx_nr);
 	xdp_status->if_status[xdp_status->rx_nr].list = NULL;
 	xdp_status->rx_ports[xdp_status->rx_nr] = key.ifindex;
 	xdp_status->rx_nr++;
@@ -227,15 +255,14 @@ int if_stats(struct xdp_status *xdp_status, const char *name)
 	int id, ifindex;
 
 	if (!xdp_status->rx_nr) {
-		fprintf(stderr, "no attached interface\n");
+		srv_err("no attached interface\n");
 		return -1;
 	}
 
 	id = __lookup_if_by_name(xdp_status->rx_ports, name, &ifindex);
 	if (id == -1) {
 		if (ifindex)
-			fprintf(stderr, "interface %s:%d not attached\n",
-				name, ifindex);
+			srv_err("interface %s:%d not attached\n",name, ifindex);
 		return -1;
 	}
 	__if_stats(xdp_status, id);
@@ -268,17 +295,17 @@ struct rule *__rule_lookup(struct xdp_status *xdp_status, const char *in_dev,
 {
 	*id = -1;
 	if (inet_pton(AF_INET, saddr, &key->saddr) != 1) {
-		fprintf(stderr, "invalid addrss %s\n", saddr);
+		srv_err("invalid addrss %s\n", saddr);
 		return NULL;
 	}
 	if (!xdp_status->rx_nr) {
-		fprintf(stderr, "no attached interface\n");
+		srv_err("no attached interface\n");
 		return NULL;
 	}
 	*id = __lookup_if_by_name(xdp_status->rx_ports, in_dev, &key->ifindex);
 	if (*id == -1) {
 		if (key->ifindex)
-			fprintf(stderr, "interface %s:%d not attached\n",
+			srv_err("interface %s:%d not attached\n",
 				in_dev, key->ifindex);
 		return NULL;
 	}
@@ -296,8 +323,7 @@ void rule_add(struct xdp_status *xdp_status, const char *in_dev,
 
 	rule = __rule_lookup(xdp_status, in_dev, saddr, &id, &key);
 	if (rule) {
-		fprintf(stderr, "Rule %x:%x already exists\n", key.ifindex,
-		        key.saddr);
+		srv_err("Rule %x:%x already exists\n", key.ifindex, key.saddr);
 		return;
 	}
 	if (id == -1)
@@ -308,35 +334,34 @@ void rule_add(struct xdp_status *xdp_status, const char *in_dev,
 	for (i = 1; i < xdp_status->cpu_nr; i++)
 		entry[i].ifindex = entry[0].ifindex;
 	if (!entry[0].ifindex) {
-		fprintf(stderr, "Can't find egress interface %s\n", out_dev);
+		srv_err("Can't find egress interface %s\n", out_dev);
 		return;
 	}
 
 	if (bpf_map_update_elem(xdp_status->maps[MAP_TX], &entry[0].ifindex,
 				&entry[0].ifindex, BPF_ANY)) {
-		fprintf(stderr, "can't add tx port %s:%d %d:%s\n", out_dev,
+		srv_err("can't add tx port %s:%d %d:%s\n", out_dev,
 		        entry[0].ifindex, errno, strerror(errno));
 		return;
 	}
 
 	if (bpf_map_update_elem(xdp_status->maps[MAP_EGRESS], &key, entry,
 	                        BPF_ANY)) {
-		fprintf(stderr, "Can't add rule %x:%x %d:%s\n", key.ifindex,
+		srv_err("Can't add rule %x:%x %d:%s\n", key.ifindex,
 		        key.saddr, errno, strerror(errno));
 		return;
 	}
 
 	rule = calloc(1, sizeof(struct rule));
 	if (!rule) {
-		fprintf(stderr, "Can't alloc rule %x:%x\n", key.ifindex,
-		        key.saddr);
+		srv_err("Can't alloc rule %x:%x\n", key.ifindex, key.saddr);
 		return;
 	}
 	rule->saddr = key.saddr;
 	rule->next = xdp_status->if_status[id].list;
 	xdp_status->if_status[id].list = rule;
 	xdp_status->if_status[id].rule_nr++;
-	printf("added rule %x:%x -> %x, if rule cnt %d\n", key.ifindex,
+	srv_out("added rule %x:%x -> %x, if rule cnt %d\n", key.ifindex,
 		key.saddr, entry[0].ifindex, xdp_status->if_status[id].rule_nr);
 }
 
@@ -350,12 +375,12 @@ void rule_del(struct xdp_status *xdp_status, const char *in_dev,
 	prev = __rule_lookup(xdp_status, in_dev, saddr, &id, &key);
 	if (!prev) {
 		if (id != -1)
-			fprintf(stderr, "Can't find rule %x:%x\n",
-				key.ifindex, key.saddr);
+			srv_err("Can't find rule %x:%x\n", key.ifindex,
+				key.saddr);
 		return;
 	}
 	if (!prev->next) {
-		fprintf(stderr, "internal error: bad lookup!?!\n");
+		srv_err("internal error: bad lookup!?!\n");
 		return;
 	}
 
@@ -363,7 +388,7 @@ void rule_del(struct xdp_status *xdp_status, const char *in_dev,
 	key.ifindex = xdp_status->rx_ports[id];
 	key.saddr = rule->saddr;
 	if (bpf_map_delete_elem(xdp_status->maps[MAP_EGRESS], &key)) {
-		fprintf(stderr, "Can't delete rule %x:%x %d:%s\n", key.ifindex,
+		srv_err("Can't delete rule %x:%x %d:%s\n", key.ifindex,
 		        key.saddr, errno, strerror(errno));
 		return;
 	}
@@ -371,7 +396,7 @@ void rule_del(struct xdp_status *xdp_status, const char *in_dev,
 	xdp_status->if_status[id].rule_nr--;
 	prev->next = rule->next;
 	free(rule);
-	printf("delered rule %x:%x, if rule cnt %d\n", key.ifindex,
+	srv_out("delered rule %x:%x, if rule cnt %d\n", key.ifindex,
 		key.saddr, xdp_status->if_status[id].rule_nr);
 }
 
@@ -382,6 +407,7 @@ void cleanup(struct xdp_status *xdp_status)
 	if (!xdp_status->cleanup)
 		return;
 
+	xdp_status->use_stdio = true;
 	for (id = MAX_IF - 1; id >= 0; --id) {
 		if (!xdp_status->rx_ports[id])
 			continue;
@@ -391,66 +417,119 @@ void cleanup(struct xdp_status *xdp_status)
 	}
 }
 
+bool process_line(struct xdp_status *xdp_status, bool use_stdio)
+{
+	char tokens[4][MAX_TOKEN + 1];
+	char line[MAX_LINE];
+	int ret, ntoken, i;
+
+	for (i = 0; i < MAX_LINE - 1; i++) {
+		ret = read(use_stdio ? 0 : xdp_status->cl_socket, &line[i], 1);
+		if (ret <= 0)
+			return false;
+		if (line[i] == '\n')
+			break;
+	}
+
+	line[i]=0;
+	ntoken = sscanf(line,"%"XSTR(MAX_TOKEN)"s %"XSTR(MAX_TOKEN)"s "
+			    "%"XSTR(MAX_TOKEN)"s %"XSTR(MAX_TOKEN)"s",
+			    tokens[0], tokens[1],
+			    tokens[2], tokens[3]);
+	if (ntoken < 1)
+		return true;
+
+	xdp_status->use_stdio = use_stdio;
+	if (!strcmp(tokens[0], "quit")) {
+		xdp_status->interrupted = true;
+	} else if (!strcmp(tokens[0], "attach")) {
+		if (ntoken != 2) {
+			srv_err("syntax: attach <device>\n");
+			return true;
+		}
+		if_attach(xdp_status, tokens[1]);
+	} else if (!strcmp(tokens[0], "detach")) {
+		if (ntoken != 2) {
+			srv_err("syntax: detach <device>\n");
+			return true;
+		}
+		if_detach(xdp_status, tokens[1]);
+	} else if (!strcmp(tokens[0], "stats")) {
+		if (ntoken != 2) {
+			srv_err("syntax: stats <device>\n");
+			return true;
+		}
+		if_stats(xdp_status, tokens[1]);
+	} else if (!strcmp(tokens[0], "add")) {
+		if (ntoken != 4) {
+			srv_err("syntax: add <in dev> <ip> <out dev>\n");
+			return true;
+		}
+		rule_add(xdp_status, tokens[1], tokens[2], tokens[3]);
+	} else if (!strcmp(tokens[0], "del")) {
+		if (ntoken != 3) {
+			srv_err("syntax: del <in dev> <ip>\n");
+			return true;
+		}
+		rule_del(xdp_status, tokens[1], tokens[2]);
+	} else {
+		srv_out("unknown command %s\n", tokens[0]);
+	}
+
+	fflush(__file(xdp_status, stdout));
+	return true;
+}
+
 int main(int argc, char *argv[])
 {
 	struct xdp_status xdp_status;
-	char tokens[4][MAX_TOKEN + 1];
-	char linebuf[MAX_LINE];
-	char *line;
+	int max_fd, ret;
+	fd_set rfds;
 
 	init(&xdp_status);
 
-	for (;;) {
-		int ntoken;
+	write(1, ">> ", 3);
+	while (!xdp_status.interrupted) {
 
-		printf(">> ");
-		line = fgets(linebuf, MAX_LINE - 1, stdin);
-		if  (xdp_status.interrupted || !line)
-			break;
-
-		ntoken = sscanf(line,"%"XSTR(MAX_TOKEN)"s %"XSTR(MAX_TOKEN)"s "
-				    "%"XSTR(MAX_TOKEN)"s %"XSTR(MAX_TOKEN)"s",
-				    tokens[0], tokens[1],
-				    tokens[2], tokens[3]);
-		if (ntoken < 1)
-			continue;
-
-		if (!strcmp(tokens[0], "quit")) {
-			break;
-		} else if (!strcmp(tokens[0], "nocleanup")) {
-			xdp_status.cleanup = false;
-		} else if (!strcmp(tokens[0], "attach")) {
-			if (ntoken != 2) {
-				fprintf(stderr, "syntax: attach <device>\n");
-				continue;
-			}
-			if_attach(&xdp_status, tokens[1]);
-		} else if (!strcmp(tokens[0], "detach")) {
-			if (ntoken != 2) {
-				fprintf(stderr, "syntax: detach <device>\n");
-				continue;
-			}
-			if_detach(&xdp_status, tokens[1]);
-		} else if (!strcmp(tokens[0], "stats")) {
-			if (ntoken != 2) {
-				fprintf(stderr, "syntax: stats <device>\n");
-				continue;
-			}
-			if_stats(&xdp_status, tokens[1]);
-		} else if (!strcmp(tokens[0], "add")) {
-			if (ntoken != 4) {
-				fprintf(stderr, "syntax: add <in dev> <ip> <out dev>\n");
-				continue;
-			}
-			rule_add(&xdp_status, tokens[1], tokens[2], tokens[3]);
-		} else if (!strcmp(tokens[0], "del")) {
-			if (ntoken != 3) {
-				fprintf(stderr, "syntax: del <in dev> <ip>\n");
-				continue;
-			}
-			rule_del(&xdp_status, tokens[1], tokens[2]);
+		FD_ZERO(&rfds);
+		FD_SET(0, &rfds);
+		if (xdp_status.cl_socket != -1) {
+			FD_SET(xdp_status.cl_socket, &rfds);
+			max_fd = xdp_status.cl_socket;
 		} else {
-			printf("unknown command %s\n", tokens[0]);
+			FD_SET(xdp_status.ctr_socket, &rfds);
+			max_fd = xdp_status.ctr_socket;
+		}
+
+		ret = select(max_fd + 1, &rfds, NULL, NULL, NULL);
+		if (ret == -1) {
+			perror("select");
+			continue;
+		}
+
+		if (FD_ISSET(0, &rfds)) {
+			if (!process_line(&xdp_status, true))
+				xdp_status.interrupted = true;
+			write(1, ">> ", 3);
+		} else if (FD_ISSET(xdp_status.cl_socket, &rfds)) {
+			if (!process_line(&xdp_status, false)) {
+				fclose(xdp_status.cl_file);
+				xdp_status.cl_socket = -1;
+			}
+		} else if (FD_ISSET(xdp_status.ctr_socket, &rfds)) {
+			xdp_status.cl_socket = accept(xdp_status.ctr_socket,
+						      NULL, NULL);
+			if (xdp_status.cl_socket == -1) {
+				perror("accept");
+				continue;
+			}
+			xdp_status.cl_file = fdopen(xdp_status.cl_socket, "a+");
+			if (!xdp_status.cl_file) {
+				perror("fdopen");
+				fclose(xdp_status.cl_file);
+				xdp_status.cl_socket = -1;
+				continue;
+			}
 		}
 	}
 
